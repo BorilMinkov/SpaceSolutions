dba676238f933fb2f1ac7f27e9e6ff99b0dbc094
Boril Minkov <BorilBMinkov@outlook.com>
2024-04-06 20:34:14 +0300

    Created method to get csv Files from path

---
+++ SpaceSolutions/src/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SpaceSolutions.src.csvHandler;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Choose Language/ Sprache");
+string? input = Console.ReadLine();
+
+Console.WriteLine("enter path");
+string path = Console.ReadLine();
+CsvHandler csvHandler = new CsvHandler();
+String[] files = csvHandler.getCsvFiles(path);
+Console.WriteLine($"Found:");
+foreach (String file in files)
+{
+    Console.WriteLine(file);
+}
---
+++ SpaceSolutions/src/csvHandler/csvHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceSolutions.src.csvHandler
+{
+    public class CsvHandler
+    {
+        /*
+        string folderPath;
+        string senderEmail;
+        string senderPassord;
+        string receiverEmail;
+        */
+
+        public string[] getCsvFiles(String folderPath)
+        {
+            string[] files = Directory.GetFiles(folderPath, "*.csv");
+            return files;
+        }
+
+    }
+}
