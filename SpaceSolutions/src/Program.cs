75d9716c0c4e7d3560dc853a9f2129bb970df11b
Boril Minkov <BorilBMinkov@outlook.com>
2024-04-07 19:01:23 +0300

    Created method to get data from csv file

--- SpaceSolutions/src/Program.cs
+++ SpaceSolutions/src/Program.cs
@@ -2,15 +2,26 @@
 using SpaceSolutions.src.csvHandler;
 
 Console.WriteLine("Hello, World!");
+
 Console.WriteLine("Choose Language/ Sprache");
 string? input = Console.ReadLine();
+if (input == "")
+{
+    input = "english";
+}
 
 Console.WriteLine("enter path");
 string path = Console.ReadLine();
+if (path == "")
+{
+    path = "C:\\Users\\steel\\Desktop\\WeatherStations";
+}
+
 CsvHandler csvHandler = new CsvHandler();
 String[] files = csvHandler.getCsvFiles(path);
-Console.WriteLine($"Found:");
+List<List<DayModel>> csvDataList = new List<List<DayModel>>();
 foreach (String file in files)
 {
-    Console.WriteLine(file);
+    List<DayModel> csvData = csvHandler.getCsvData(file);
+    csvDataList.Add(csvData);
 }
--- SpaceSolutions/src/csvHandler/csvHandler.cs
+++ SpaceSolutions/src/csvHandler/csvHandler.cs
@@ -1,7 +1,13 @@
-﻿using System;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SpaceSolutions.src.csvHandler
@@ -21,5 +27,26 @@ namespace SpaceSolutions.src.csvHandler
             return files;
         }
 
+        public List<DayModel> getCsvData(String filePath)
+        {
+            String delimiter = ",";
+            List<DayModel> dayList = new List<DayModel>();
+            using (StreamReader streamReader = new StreamReader(filePath))
+            {
+                using (CsvReader csvReader = new CsvReader(streamReader, new CsvConfiguration(CultureInfo.InvariantCulture) { Delimiter = delimiter }))
+                {
+                    //do something with csv
+                    {
+                        IEnumerable<DayModel> dayRecords = csvReader.GetRecords<DayModel>();
+                        foreach (DayModel day in dayRecords)
+                        {
+                            dayList.Add(day);
+                        }
+                    }
+                }
+                return dayList;
+
+            }
+        }
     }
 }
