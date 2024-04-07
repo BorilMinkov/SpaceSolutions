005fbbd07c062724e06282941f90954b783fa425
Boril Minkov <BorilBMinkov@outlook.com>
2024-04-07 19:00:24 +0300

    Created model for days

---
+++ SpaceSolutions/src/csvHandler/DayModel.cs
@@ -0,0 +1,34 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceSolutions.src.csvHandler
+{
+    public class DayModel
+    {
+
+        [Name("Day/Parameter")]
+        public int Day { get; set; }
+
+        [Name("Temperature (C)")]
+        public int Temperature { get; set; }
+
+        [Name("Wind (m/s)")]
+        public int Wind { get; set; }
+
+        [Name("Humidity (%)")]
+        public int Humidity { get; set;}
+
+        [Name("Precipitation (%)")]
+        public int Precipation { get; set; }
+
+        [Name("Lightning")]
+        public string Lightning { get; set; }
+
+        [Name("Clouds") ]
+        public string Clouds { get; set; }
+    }
+}
